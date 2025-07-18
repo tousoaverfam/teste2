99537d754de21c6456a58c5192c2386dfad64598
tousoaverfam <apenasaobservar@gmail.com>
2025-07-18 15:54:58 +0100

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,3 +2,23 @@ function scrollToSection() {
   const section = document.getElementById("conteudo");
   section.scrollIntoView({ behavior: "smooth" });
 }
+
+function addItem() {
+  const input = document.getElementById("alimentoInput");
+  const value = input.value.trim();
+  if (value === "") return;
+
+  const container = document.getElementById("alimentosContainer");
+
+  const tag = document.createElement("div");
+  tag.className = "tag";
+  tag.innerHTML = `${value} <button class="remove-btn" onclick="removeItem(this)">-</button>`;
+
+  container.appendChild(tag);
+  input.value = "";
+}
+
+function removeItem(btn) {
+  const tag = btn.parentElement;
+  tag.remove();
+}
